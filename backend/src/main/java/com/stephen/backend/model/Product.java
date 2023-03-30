ebdbf8aa2c4f31b066f74a7947140373a482cbb7
Stephen Chen <schen5632@outlook.com>
2023-03-29 20:05:09 -0400

    update

--- backend/src/main/java/com/stephen/backend/model/Product.java
+++ backend/src/main/java/com/stephen/backend/model/Product.java
@@ -4,15 +4,14 @@ import java.math.BigDecimal;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 public class Product {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String priceId;
